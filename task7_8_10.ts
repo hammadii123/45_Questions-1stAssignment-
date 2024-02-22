f42bb47edb516e16458fa37d348188caef0bdfa9
HammadMustafa <125297432+hammadii123@users.noreply.github.com>
2024-02-22 10:26:33 +0500

    Add files via upload

---
+++ task7_8_10.ts
@@ -0,0 +1,5 @@
+//addition, subtraction, multiplication, and division operations
+console.log("Addition:"+(5+3));
+console.log("Subtraction:"+(14-6));
+console.log("Multiplication:"+(2*4));
+console.log("Divison:"+(32/4));
