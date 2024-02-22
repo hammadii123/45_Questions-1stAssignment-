7c1f6ff4d6ec125bd7d316e3117c3069049f5f7d
HammadMustafa <125297432+hammadii123@users.noreply.github.com>
2024-02-22 10:26:43 +0500

    Add files via upload

---
+++ task9_10.ts
@@ -0,0 +1,6 @@
+//I have stored my favourite num in the variable named "f_num";
+let f_num: number = 9;
+//then wrote the msg by using value in "f_num" in a variable named msg;
+const msg = `My favorite number is ${f_num}.`;
+//printed the msg;
+console.log(msg);
