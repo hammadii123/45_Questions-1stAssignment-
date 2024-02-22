7f46cc3524a722fc769ec86b448d4b45bfb8bd1c
HammadMustafa <125297432+hammadii123@users.noreply.github.com>
2024-02-22 10:48:07 +0500

    Update task_23.ts

--- task_23.ts
+++ task_23.ts
@@ -8,14 +8,13 @@ console.log(number === 7);
 console.log("Is the number not equal to 5? I predict True.");
 console.log(number !== 5);
 
-// ... (similar tests)
 
 
 console.log("Is the string 'apple' equal to 'orange'? I predict False.");
 //@ts-ignore
-console.log('apple' === 'orange'); // Error here
+console.log('apple' === 'orange'); // Error occurs if we dont write "//@ts-ignore"
 
-// Test 10: Is the boolean true not equal to false?
+// Test 10:boolean not equals to false?
 console.log("Is the boolean true not equal to false? I predict True.");
 //@ts-ignore
-console.log(true !== false); // Error here
+console.log(true !== false); // Error occurs
